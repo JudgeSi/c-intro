5e9ad840791ef30fda8b940f4998f4ed9ec484b2
si <schaefer.simon@gmx.net>
2020-04-20 13:57:51 +0200

    Revert "mend"
    This reverts commit eb9df07741dab0d48e6f200b9f86c7c43fd6541d.

---
+++ course/execute-c.sh
@@ -0,0 +1,7 @@
+# This script will compile and execute any c files passed as input.
+# This program should be saved under an alias for convenience.
+# This script hould be complied so that it does not need to be called through "sh scirpt.sh"
+# This script could be written in c and compiled.
+
+# the dot infornt of the slash means that the program to be executed resides in the current directory.
+./
---
+++ get-c-help.sh
@@ -0,0 +1,4 @@
+# With the following command one can get help on C-library functions (specified in the -s3 parameter)
+# TODO finish this function.
+
+man -s3 #funktionsname
