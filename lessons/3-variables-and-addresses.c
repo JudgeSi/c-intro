a2c6fb0c86e1c1e4ceb33f6988cb0d375172001c
si <schaefer.simon@gmx.net>
2020-04-20 15:07:25 +0200

    introduces pointer math

--- lessons/3-variables-and-addresses.c
+++ lessons/3-variables-and-addresses.c
@@ -1,7 +1,9 @@
 /**
-	Variables and Addresses
+	Memory
 
-	RAM is sepereated into different sections which are addessable.
+    	This section is about memory and goes into detail about variables, addresses and pointers.
+
+	RAM (memory) is sepereated into different sections which are addessable.
 	The value of a variable is the same as the value at a definitive address.
 	Different types of data need a differently siced bytes to be saved, suchas:
 		* boolean:  1 bit (0:=false, 1:= true)
@@ -11,11 +13,44 @@
 
 		TODO more types
 **/
+#include <stdio.h>
 
+void variables_and_addresses(void)
 {
   int i;
   // somewhere in memory a place is allocated which is big enough to store an int.
   // this place is the address of a: &a
+  
+  // unitialized values just contain a garbage value
+  printf("This is the unitialized value i now %i \n", i); 
+  // if we set a proper value it gets stored in i
+  i = 1;
+  printf("i equals %i now \n", i);  
+
+  // this value is stored at somewhere in RAM. We can figure out the bytes required to store a value
+  // through the sizeof(i); operation.
+  long byteLengthForI = sizeof(i);
+  printf("The size to store i is %li bytes \n", sizeof(i));
+
+  // these bytes are located somewhere in memory. This location is called an address and is identitfied by the number of the starting byte.
+  // With the &-opeator we can request the value of any variable.
+  printf("This is the location of i currently: %x \n", &i);
+
+  // This value of an address is also called a pointer. 
+  // In c we have a specific datatype for a pointer. Pointers are marked with a *
+  // Furthermore a pointer must have a specific type to which it points.
+  int *pointer = &i;
+  printf("The value of this pointer is the same as &i: %x  \n", pointer);
+
+  // since pointers are just plain numbers for bytes in memory we can address anything we want in memory by just adding or subtracting from a pointer.
+  printf("The first byte in i: %c \n", pointer + 1);
+
+
+
+  // excursion: byte limitation:
+  // this is also why we cannot store a number which is larger than an int in i. 
+  i = 100000000000;
+  printf("Setting i to one-hundred-billion does not work: i = %i \n", i);
   
   int *int_pointer;
   int *char_pointer;
@@ -35,4 +70,10 @@
 	
   // parameters * TODO 
 
+}
+
+int main(void)
+{
+   variables_and_addresses();
+   return 0;
 }
